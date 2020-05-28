5f9937645cd8c9dc79c099f8d20cbd6d720ff7ab
Ishav bhatt <ishavbhattkhaniyara.com>
2020-05-28 23:24:10 +0530

    add about component

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import "../stylesheets/_about.scss";
+
+export default function About() {
+  return (
+    <div className="aboutpage-section">
+      <h1>abooout pppppppppppppage</h1>
+    </div>
+  );
+}
